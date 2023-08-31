d0686b5035f4e8f9119166cc6a23d76c34760d91
גלעד סקייט - אחדות ישראל <gilads@achdut.co.il>
2023-08-31 11:52:50 +0300

    refactor

--- lang_sayer.py
+++ lang_sayer.py
@@ -4,66 +4,67 @@ import time
 import threading
 from win32com.client import Dispatch
 import pythoncom
+import argparse
 
 # Constants
-cooldown_duration = 10  # Cooldown duration in seconds
+COOLDOWN_DURATION = 10  # Cooldown duration in seconds
 
-language_names = {
+LANGUAGE_NAMES = {
     0x0409: "English",   # English - United States
     0x040D: "Hebrew",    # Hebrew
     # Add more language IDs and their names as needed
 }
 
-last_key_press_time = [0, 1000]
+class LanguageDetector:
+    def __init__(self, debug=False):
+        self.debug = debug
+        self.current_language = ""
+        self.last_key_press_times = [0, 1000]
+        self.speak_lock = threading.Lock()
+        self.speak_engine = Dispatch("SAPI.SpVoice")
 
-# Initialize global variables
-current_language = ""
-speak_lock = threading.Lock()
-speak_engine = Dispatch("SAPI.SpVoice")
+    def debug_print(self, message):
+        if self.debug:
+            timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+            print(f"[{timestamp}] [DEBUG] {message}")
 
-# make a list variable with 2 elements and assign 0 to both
-# last_key_press_time = [0, 0]
+    def speak_language(self, language):
+        current_time = time.time()
+        if self.current_language != language or self.last_key_press_times[1] - self.last_key_press_times[0] >= COOLDOWN_DURATION:
+            self.current_language = language
+            pythoncom.CoInitialize()
+            with self.speak_lock:
+                self.speak_engine.Speak(language)
+            self.debug_print(f"Speaking language: {language}")
 
+    def get_keyboard_layout_language(self):
+        user32 = ctypes.windll.user32
+        hwnd = user32.GetForegroundWindow()
+        thread_id = user32.GetWindowThreadProcessId(hwnd, 0)
+        layout_id = user32.GetKeyboardLayout(thread_id)
+        lang_id = layout_id & 0xFFFF
+        return LANGUAGE_NAMES.get(lang_id, "Unknown")
 
-def speak_language(language):
-    global current_language
+    def key_event(self, event):
+        if event.event_type == keyboard.KEY_UP:
+            self.last_key_press_times[0] = self.last_key_press_times[1]
+            self.last_key_press_times[1] = time.time()
+            self.update_history_window(event.name)  # Pass the detected key to the function
 
-    current_time = time.time()
-    # and current_time - last_key_press_time >= cooldown_duration:
-    if current_language != language or last_key_press_time[1]-last_key_press_time[0] >= cooldown_duration:
-        current_language = language
-        pythoncom.CoInitialize()
-        with speak_lock:
-            speak_engine.Speak(language)
+    def update_history_window(self, key_name):
+        language_display = self.get_keyboard_layout_language()
+        self.speak_language(language_display)
+        self.debug_print(f"Detected language: {language_display}, Key: {key_name}")  # Include the detected key
 
-
-def get_keyboard_layout_language():
-    user32 = ctypes.windll.user32
-    hwnd = user32.GetForegroundWindow()
-    thread_id = user32.GetWindowThreadProcessId(hwnd, 0)
-    layout_id = user32.GetKeyboardLayout(thread_id)
-    lang_id = layout_id & 0xFFFF
-    return language_names.get(lang_id, "Unknown")
-
-
-def key_event(event):
-
-    if event.event_type == keyboard.KEY_UP:
-        last_key_press_time[0] = last_key_press_time[1]
-        last_key_press_time[1] = time.time()
-        # print(time.time(), threading.current_thread().name)
-        update_history_window()
-
-
-def update_history_window():
-    language_display = get_keyboard_layout_language()
-    speak_language(language_display)
-
-
-def main():
-    keyboard.hook(key_event)
-    keyboard.wait()  # Keeps the script running
-
+    def start(self):
+        self.debug_print("Language detector started")
+        keyboard.hook(self.key_event)
+        keyboard.wait()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Language Detector")
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode")
+    args = parser.parse_args()
+
+    detector = LanguageDetector(debug=args.debug)
+    detector.start()
